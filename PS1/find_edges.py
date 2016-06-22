d714e3529b5337a0b95001052e58532e802a235f
Josh Miller <jxm7374@rit.edu>
2016-06-22 00:32:17 -0400

    More progress

--- PS1/find_edges.py
+++ PS1/find_edges.py
@@ -11,28 +11,35 @@ cv2.imshow('image',inputImage);
 cv2.waitKey(0);
 cv2.destroyAllWindows();
 #Find edges
-edges = cv2.Canny(inputImage,100,200);
+edges = cv2.Canny(cv2.cvtColor(inputImage, cv2.COLOR_BGR2GRAY),100,200);
 #Show Edges of input image
 cv2.imshow('image',edges);
 cv2.waitKey(0);
 cv2.destroyAllWindows();
 #Get Hough accumulator array of edge image
 H = hough.hough_lines(edges);
-cv2.imshow('image',H);
-cv2.waitKey(0);
-cv2.destroyAllWindows();
 lines = inputImage;
-peaks = hough.find_peaks(H,4);
-for theta,rho in peaks:
-	a = np.cos(theta);
-	b = np.sin(theta);
-	x0 = a*rho;
-	y0 = b*rho;
-	x1 = int(x0 + 1000*(-b));
-	y1 = int(x0 + 1000*(a));
-	x2 = int(x0 - 1000*(-b));
-	y2 = int(x0 - 1000*(a));
-	cv2.line(lines,(x1,y1),(x2,y2),(0,255,0),2);
+peaks = hough.find_peaks(H,6);
+peaks2 = cv2.HoughLines(edges,1,np.pi/180,200);
+for rho,theta in peaks:
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+    cv2.line(lines,(x1,y1),(x2,y2),(255,0,0),2)
+    # a = np.cos(theta+1.57079)
+    # b = np.sin(theta+1.57079)
+    # x0 = a*rho
+    # y0 = b*rho
+    # x1 = int(x0 + 1000*(-b))
+    # y1 = int(y0 + 1000*(a))
+    # x2 = int(x0 - 1000*(-b))
+    # y2 = int(y0 - 1000*(a))
+    # cv2.line(lines,(x1,y1),(x2,y2),(255,0,0),2)
 cv2.imshow('image',lines);
 cv2.waitKey(0);
 cv2.destroyAllWindows();
--- PS1/hough.py
+++ PS1/hough.py
@@ -4,32 +4,32 @@ import math
 #Get Hough Accumulator Array of edge image
 def hough_lines(edges):
 	d = int(math.sqrt((edges.shape[0] * edges.shape[0]) + (edges.shape[1] * edges.shape[1])));
-	D = d ;
+	D = d*2 + 1 ;
 	H = np.zeros(shape=(D,180))
 	for x in range(edges.shape[0]):
 		for y in range(edges.shape[1]):
-			if edges[x,y] != 0:
+			if edges[x,y] == 255:
 				for theta in range(180):
-					rtheta = math.radians(theta);
-					temp = int(x*np.cos(rtheta) - y*np.sin(rtheta));
-					if temp < 0:
-						temp += d;
-					H[temp,theta] += 1;
+					rtheta = (np.pi / 180) * (theta-1);
+					temp = int((x*np.cos(rtheta) - y*np.sin(rtheta)) );
+					temp = (temp + d) % d;
+					H[temp,(theta-1)%180] += 1;
 	return H;
 
+#Find peaks in Hough Accumularot Array
 def find_peaks(H,num_peaks):
 	peaks = [];
 	temp_max = 0;
-	temp_coord = {};
+	temp_coord = [];
 	for i in range(num_peaks):
 		for x in range(H.shape[0]):
 			for y in range(H.shape[1]):
 				if H[x,y] > temp_max:					
-					t = {x,y};
+					t = [x,y];
 					if t not in peaks:
 						temp_max = H[x,y];
 						temp_coord = t;
 		peaks.append(temp_coord);
 		temp_max = 0;
-		temp_coord = {};
+		temp_coord = [];
 	return peaks;
