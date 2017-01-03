7ce532b001df577ff41f492a38f916a907214c00
Josh <jxm7374@rit.edu>
2017-01-02 23:21:38 -0500

    Moved files to input

--- PS0/ps0.py
+++ PS0/ps0.py
@@ -6,8 +6,8 @@ import numpy as np
 import utils
 
 #Number 1:
-im1 = cv2.imread('img1.png');
-im2 = cv2.imread('img2.png');
+im1 = cv2.imread('Input/img1.png');
+im2 = cv2.imread('Input/img2.png');
 l1 =  im1.shape[0];
 w1 =  im1.shape[1];
 l2 = im2.shape[0];
@@ -19,10 +19,10 @@ utils.showAndWait(im1)
 im1[:,:,0] = im1[:,:,2];
 im1[:,:,2] = temp;
 utils.showAndWait(im1)
-cv2.imwrite("swap red and blue.jpg",im1);
+cv2.imwrite("Output/swap red and blue.jpg",im1);
 
 r,g,b = getRGBMonochromes(im1)
-cv2.imwrite('green_monochrome.png',g);
+cv2.imwrite('Output/green_monochrome.png',g);
 #show green monochrome
 utils.showAndWait(r)
 #show red monochrome
@@ -41,5 +41,5 @@ cent = green[((l1/2)-50):((l1/2)+50),((w1/2)-50):((w1/2)+50)];
 utils.showAndWait(cent)
 green2[l2/2-50:l2/2+50,w2/2-50:w2/2+50] = cent;
 utils.showAndWait(green2)
-cv2.imwrite('weird_center_thing.png',green2);
+cv2.imwrite('Output/weird_center_thing.png',green2);
 
